c4068815411f24381bad85ea616fbb118147c669
Erik Hernandez <erik.hernandez.enriquez@gmail.com>
2021-01-16 15:59:13 -0600

    PyBank Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,8 +25,50 @@ with open(csvpath) as csvfile:
         number_months.append(row[0])
         profit.append(int(row[1]))
 
+#Calculate the number of months in the data
 Total_Months = len(number_months)
-Total_Amount = sum(profit)
-print(f"Total Months: {Total_Months}" )
-print(Total_Amount)
 
+#Calculate the sum of profits
+Total_Amount = sum(profit)
+
+#Calculate de Differences between each month and then the average change
+Differences = [profit[n]-profit[n-1] for n in range(1,len(profit))]
+Average_Change = round(sum(Differences)/len(Differences),2)
+
+#Calculate the greatest increase in losses
+Max_Increase = max(Differences)
+
+#Obtain the index of the greatest loss to get the month of the year
+index1= Differences.index(Max_Increase)
+month_max_profit = number_months[index1+1]
+
+#Calculate the minimum increase in losses
+Max_Decrease = min(Differences)
+
+#Obtain the index of the greatest loss to get the month of the year
+index2= Differences.index(Max_Decrease)
+month_min_profit = number_months[index2+1]
+
+combined_list = zip(number_months,profit,Differences)
+list(combined_list)
+
+#Print all analysis to terminal
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {Total_Months}" )
+print(f"Total: ${Total_Amount}")
+print(f"Average Change: ${Average_Change}")
+print(f"Greatest Increase in Profits: {month_max_profit} (${Max_Increase})")
+print(f"Greatest Decrease in Profits: {month_min_profit} (${Max_Decrease})")
+
+#Export Data to TXT FILE inside Analysis Folder
+txtpath = os.path.join( 'Analysis', 'financial_analysis.txt')
+f=open(txtpath,"w+")
+f.write("Financial Analysis\r\n")
+f.write("---------------------\r\n")
+f.write(f"Total Months: {Total_Months}\r\n" )
+f.write(f"Total: ${Total_Amount}\r\n")
+f.write(f"Average Change: ${Average_Change}\r\n")
+f.write(f"Greatest Increase in Profits: {month_max_profit} (${Max_Increase})\r\n")
+f.write(f"Greatest Decrease in Profits: {month_min_profit} (${Max_Decrease})\r\n")
+f.close()
